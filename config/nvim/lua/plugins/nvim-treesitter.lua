7a5ab11cf1ba657b77be3683a516cf918857a93d
gmmyung <gmmyung@kaist.ac.kr>
2026-06-30 01:56:47 +0900

    nvim: enable treesitter highlighting

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,5 +5,14 @@ return {
 	main = "nvim-treesitter",
 	opts = {
 		install_dir = vim.fn.stdpath("data") .. "/site",
-	}
+	},
+	config = function(_, opts)
+		require("nvim-treesitter").setup(opts)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
+		})
+	end,
 }
